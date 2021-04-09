784c1773f1759e0d1371f31027a4483759e584ee
nghia <nqnghia285@gmail.com>
2021-04-09 12:14:48 +0700

    Setup Package

--- index.d.ts
+++ index.d.ts
@@ -11,7 +11,7 @@ import { Socket } from 'socket.io';
  * @param token
  * @returns UserInfo | undefined
  */
-export function authenticateUser(token: string): UserInfo | undefined;
+export declare function authenticateUser(token: string): UserInfo | undefined;
 
 /**
  * @method createToken: Return an encoded token if the function is not error, otherwise return undefined
@@ -19,18 +19,21 @@ export function authenticateUser(token: string): UserInfo | undefined;
  * @param option
  * @returns string | undefined
  */
-export function createToken(payload: string | object | Buffer, option?: SignOptions | undefined): string | undefined;
+export declare function createToken(
+  payload: string | object | Buffer,
+  option?: SignOptions | undefined,
+): string | undefined;
 
 /**
  * @method authenticateUserFromReq: Return payload if token is valid, otherwise return undefined
  * @param req
  * @returns UserInfo | undefined
  */
-export function authenticateUserFromReq(req: Request | RequestType): UserInfo | undefined;
+export declare function authenticateUserFromReq(req: Request | RequestType): UserInfo | undefined;
 
 /**
  * @method authenticateUserFromSocket
  * @param socket
  * @returns UserInfo | undefined
  */
-export function authenticateUserFromSocket(socket: Socket): UserInfo | undefined;
+export declare function authenticateUserFromSocket(socket: Socket): UserInfo | undefined;
